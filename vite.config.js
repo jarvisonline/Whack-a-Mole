92724bae7c5a94c55c95485fd4bc383e369ef761
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-07-18 20:55:59 +0530

    Project10✅✅

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    open: true,
+    host: "0.0.0.0", // This option opens the browser automatically
+  },
+});
